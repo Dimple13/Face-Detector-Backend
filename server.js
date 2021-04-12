6c80c1028b86a0ef99f1cd543d57491b92044ca0
Dimple13 <59231474+Dimple13@users.noreply.github.com>
2021-04-12 23:52:38 +0530

    Revert "adding updations"
    This reverts commit 031628fc1b6a1fcfb32712c3ffcefcf6769486ba.

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const db = knex({
     }
 });
 
-app.get('/', (req, res)=> { res.send('Works!') })
+app.get('/', (req, res)=> { res.send(db.users) })
 app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
