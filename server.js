b91a73b0defbe206b586e1b69c9f9d19f1ae0b40
Dimple13 <59231474+Dimple13@users.noreply.github.com>
2021-04-13 17:41:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,9 +17,11 @@ app.use(cors());
 const db = knex({
     client: 'pg',
     connection: {
-      connectionString : process.env.DATABASE_URL,
-      ssl:true,
-    }
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+      rejectUnauthorized: false,
+      },
+      },
 });
 
 
