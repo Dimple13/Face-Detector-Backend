6e637b8702def1a5d7cddbadd0c5c47c0c47ed30
Dimple13 <59231474+Dimple13@users.noreply.github.com>
2021-04-13 13:21:16 +0530

    security issues handled-everything working normally

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,17 @@
+const { json } = require('body-parser');
+const Clarifai=require('clarifai');
+  
+const app = new Clarifai.App({
+  apiKey: '80ebc62a05334c24b6eebc6c35ac086e'
+});
+const handleApiCall=(req,res)=>{
+    app.models
+    .predict(Clarifai.FACE_DETECT_MODEL,req.body.input)
+    .then(data=>{
+        res.json(data);
+    })
+    .catch(err=>res.status(400).json('Unable to work with API'))
+}
 const handleImage=(req, res,db) => {
     const { id } = req.body;
     db('users').where('id', '=', id)
@@ -9,5 +23,6 @@ const handleImage=(req, res,db) => {
     .catch(err => res.status(400).json('unable to get entries'))
 }
 module.exports={
-    handleImage:handleImage
+    handleImage,
+    handleApiCall
 };
