925248b78bac11547a07e7fb7261177bd13151ae
Dimple13 <59231474+Dimple13@users.noreply.github.com>
2021-04-13 14:40:43 +0530

    Database addons created and host changed

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(cors());
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
+      host : 'postgresql-dimensional-91227',
       user : 'postgres',
       password : 'root',
       database : 'smartbrain'
